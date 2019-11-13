85549deddcee7ed1083117b709ac0f8402bbe94c
Bruno Padilha <brpadilha.dev@gmail.com>
2019-11-13 17:51:39 -0200

    Cria e estiliza o header da página

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
+import Header from './components/Header/index';
 
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header/> */}
+      <Header />
       <Routes />
 
-      <GlobalStyle/>
-
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
