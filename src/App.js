d11665b532dcb7d5006be0f3bd03cd128d4e9981
Bruno Padilha <brpadilha.dev@gmail.com>
2019-11-19 18:50:56 -0200

    Adiciona ao carrinho com redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import GlobalStyle from './styles/global';
 import Header from './components/Header/index';
 
 import Routes from './routes';
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
 
-      <GlobalStyle />
-    </BrowserRouter>
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
