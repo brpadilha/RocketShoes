b389116c656989b682c1c32139433e48b720279a
Bruno Padilha <brpadilha.dev@gmail.com>
2019-11-26 17:52:40 -0200

    Remove products from cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -10,7 +10,7 @@ import {
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -49,7 +49,12 @@ function Cart({ cart }) {
               </td>
               <td>
                 <div>
-                  <button type="button">
+                  <button
+                    type="button"
+                    onClick={() =>
+                      dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                    }
+                  >
                     <MdDelete size={20} color="#7159c1" />
                   </button>
                 </div>
