48e60c66faeb993e3fe733a605a42bf6353b6e03
Bruno Padilha <brpadilha.dev@gmail.com>
2019-11-28 21:48:53 -0200

    Configura o reactotron com redux

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,13 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './models/rootReducer';
 import rootSaga from './models/rootSaga';
 
-const sagaMidleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const sagaMidleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
